7a2d1d0ca137ffbe2ae401c529dfa2949e5f67c2
Isael Faria Junior <ifj@cesar.org.br>
2022-04-25 12:35:59 -0300

    Jasper Report - Linux

--- src/main/java/com/br/jasper/JasperReport.java
+++ src/main/java/com/br/jasper/JasperReport.java
@@ -3,10 +3,10 @@ package com.br.jasper;
 import com.br.jasper.service.ClientReport;
 
 public class JasperReport {
-/*
+
     public static void main(String[] args) throws Exception {
 
         ClientReport clientReport = new ClientReport();
         clientReport.execute();
-    }*/
+    }
 }
